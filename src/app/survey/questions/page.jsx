dbe045c7f224bc26f8e21da8b5fd8868aa183774
exthammad <Muhammad.Hammad@starlabs-ext.dev>
2025-05-30 12:55:08 +0500

    survey fix to add to api integration branch

---
+++ src/app/survey/questions/page.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useState } from "react";
+import StartSelling from "./pages/StartSelling";
+import SellingHistory from "./pages/SellingHistory";
+import Purpose from "./pages/Purpose";
+import CategorySelection from "./pages/CategorySelection";
+import SubcategorySelection from "./pages/SubCategorySelection";
+import Consent from "./pages/Consent";
+import { useRouter } from "next/navigation";
+
+const page = () => {
+  const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState({
+    haveProductToSell: "",
+    haveExperienceSelling: "",
+    whatBringsYouHere: "",
+    productsAndServicesToSell: "",
+    supplies: [],
+    consent: ""
+  })
+  const router = useRouter();
+
+  const goBack = () => {
+    if (step === 1) {
+      router.back()
+    }
+    else {
+      setStep(prev => prev-1)
+    }
+  }
+
+  return <div>
+    {step === 1 && <StartSelling setFormData={setFormData} setStep={setStep} goBack={goBack}/>}
+    {step === 2 && <SellingHistory setFormData={setFormData} setStep={setStep} goBack={goBack}/>}
+    {step === 3 && <Purpose setFormData={setFormData} setStep={setStep} goBack={goBack}/>}
+    {step === 4 && <CategorySelection setFormData={setFormData} setStep={setStep} goBack={goBack}/>}
+    {step === 5 && <SubcategorySelection setFormData={setFormData} setStep={setStep} formData={formData} goBack={goBack}/>}
+    {step === 6 && <Consent setFormData={setFormData} setStep={setStep} goBack={goBack}/>}
+  </div>;
+};
+
+export default page;
